e038f11abc6941ad3e48523e833ecbc042d41f42
jiokeokwuosa <okwuosachijioke@gmail.com>
2020-01-01 06:37:02 +0100

    Time to work

--- dist/server.js
+++ dist/server.js
@@ -16,8 +16,18 @@ require("./db");
 
 (0, _dotenv.config)();
 var app = (0, _express["default"])();
-var port = process.env.PORT || 5000; // app.use(cors());
+var port = process.env.PORT || 5000;
+app.use((0, _cors["default"])());
+app.options('*', (0, _cors["default"])());
 
+var allowCrossDomain = function allowCrossDomain(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+};
+
+app.use(allowCrossDomain);
 app.use(_express["default"].json());
 app.use(_express["default"].urlencoded({
   extended: true
