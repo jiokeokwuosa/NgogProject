7ff06e8244daabac7dde5107c8e6def6b62236da
jiokeokwuosa <okwuosachijioke@gmail.com>
2020-01-07 23:48:53 +0100

    Mobile responsiveness

--- client/src/components/includes/Footer.js
+++ client/src/components/includes/Footer.js
@@ -9,15 +9,15 @@ const Footer = () => {
             <h2 className="bold">CONTACT US</h2>
             <hr className="one"/><br/>
             <div className="row push">
-                <div className="col-4">
+                <div className="col-md-4">
                     <img src={require('../../assets/img/home icons.png')} alt="location"/>
                     <h6>Alfred Road,<br/>Lagos State, Nigeria</h6>
                 </div>
-                <div className="col-4">
+                <div className="col-md-4">
                     <img src={require('../../assets/img/phone icons.png')} alt="phone"/>
                     <h6>+234 700 000 0000</h6>
                 </div>
-                <div className="col-4">
+                <div className="col-md-4">
                     <img src={require('../../assets/img/message icons.png')} alt="mail"/>
                     <h6>info@nogig.com</h6>
                 </div>
